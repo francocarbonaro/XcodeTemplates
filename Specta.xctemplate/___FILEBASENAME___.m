a772f12ae08637ad6075e9cedbbdff6ceb3ebaf1
Franco Carbonaro <franco2610@gmail.com>
2014-01-21 16:47:07 -0200

    Specta file template

---
+++ Specta.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,15 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+SpecBegin(___FILEBASENAMEASIDENTIFIER___Specs)
+
+	describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
+	
+	});
+
+SpecEnd
